091880e09225f11e64f48b9ef75909eb2918ce71
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2023-12-17 19:00:14 -0800

    Update app.py

--- hugging-face/amazon-bedrock-llm/app.py
+++ hugging-face/amazon-bedrock-llm/app.py
@@ -16,9 +16,6 @@ config = {
     "top_p": 0.999,
 }
 
-#wandb.login(key = wandb_api_key)
-#wandb.init(project = "bedrock-llm", config = config)
-
 bedrock_runtime = boto3.client(
     aws_access_key_id = aws_access_key_id,
     aws_secret_access_key = aws_secret_access_key,
@@ -26,27 +23,40 @@ bedrock_runtime = boto3.client(
     region_name = "us-west-2"
 )
 
+def wandb_log(prompt, completion):
+    wandb.login(key = wandb_api_key)
+    wandb.init(project = "bedrock-llm", config = config)
+    wandb.log({"prompt": str(prompt), "completion": str(completion)})
+    wandb.finish()
+
 def invoke(prompt):
-    body = json.dumps({"prompt": "\n\nHuman: " + prompt + "\n\nAssistant: ",
-                       "max_tokens_to_sample": config["max_tokens_to_sample"],
-                       "temperature": config["temperature"],
-                       "top_k": config["top_k"],
-                       "top_p": config["top_p"],
-                       "stop_sequences": ["\n\nHuman: "]
-                      })
-    modelId = config["model"]
-    accept = "application/json"
-    contentType = "application/json"
-    #response = bedrock_runtime.invoke_model(body = body, modelId = modelId, accept = accept, contentType = contentType)
-    #response_body = json.loads(response.get("body").read())
-    #completion = response_body["completion"]
-    #wandb.log({"prompt": prompt, "completion": completion})
-    #return completion
-    return "🛑 Execution is commented out. To view the source code see https://huggingface.co/spaces/bstraehle/amazon-bedrock-llm/tree/main."
+    if (prompt == ""):
+        raise gr.Error("Prompt is required.")
+    completion = ""
+    try:
+        body = json.dumps({"prompt": "\n\nHuman: " + prompt + "\n\nAssistant: ",
+                           "max_tokens_to_sample": config["max_tokens_to_sample"],
+                           "temperature": config["temperature"],
+                           "top_k": config["top_k"],
+                           "top_p": config["top_p"],
+                           "stop_sequences": ["\n\nHuman: "]
+                          })
+        modelId = config["model"]
+        accept = "application/json"
+        contentType = "application/json"
+        response = bedrock_runtime.invoke_model(body = body, modelId = modelId, accept = accept, contentType = contentType)
+        response_body = json.loads(response.get("body").read())
+        completion = response_body["completion"]
+    except Exception as e:
+        raise gr.Error(e)
+    finally:
+        wandb_log(prompt, completion)
+    return completion
+    #return "🛑 Execution is commented out. To view the source code see https://huggingface.co/spaces/bstraehle/amazon-bedrock-llm/tree/main."
 
 description = """<a href='https://www.gradio.app/'>Gradio</a> UI using <a href='https://aws.amazon.com/bedrock/'>Amazon Bedrock</a> API 
                  with <a href='https://www.anthropic.com/'>Anthropic</a> Claude 2 foundation model. 
-                 Model performance evaluation via <a href='https://wandb.ai/bstraehle'>Weights & Biases</a>."""
+                 RAG evaluation via <a href='https://wandb.ai/bstraehle'>Weights & Biases</a>."""
 
 gr.close_all()
 demo = gr.Interface(fn=invoke, 
