4af68030a0a444b7982e1bc02e26b3c581d02f41
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2023-12-19 15:29:18 -0800

    Update app.py

--- hugging-face/openai-llm-rag/app.py
+++ hugging-face/openai-llm-rag/app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-import openai, os, time, wandb
+import langchain, openai, os, time, wandb
 
 from langchain.chains import LLMChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -30,6 +30,8 @@ MONGODB_COLLECTION_NAME = "gpt-4"
 MONGODB_COLLECTION = client[MONGODB_DB_NAME][MONGODB_COLLECTION_NAME]
 MONGODB_INDEX_NAME = "default"
 
+description = os.environ["DESCRIPTION"]
+
 config = {
     "chunk_overlap": 150,
     "chunk_size": 1500,
@@ -38,16 +40,15 @@ config = {
     "temperature": 0,
 }
 
-template = """If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. Always say 
-              "🧠 Thanks for using the app - Bernd" at the end of the answer. """
+langchain.verbose = True
 
-llm_template = "Answer the question at the end. " + template + "Question: {question} Helpful Answer: "
-rag_template = "Use the following pieces of context to answer the question at the end. " + template + "{context} Question: {question} Helpful Answer: "
+template = """If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. Always say "Thanks for using the 🧠 app - Bernd" at the end of the answer. """
 
-LLM_CHAIN_PROMPT = PromptTemplate(input_variables = ["question"], 
-                                  template = llm_template)
-RAG_CHAIN_PROMPT = PromptTemplate(input_variables = ["context", "question"], 
-                                  template = rag_template)
+llm_template = "Answer the question at the end. " + template + "Question: {question}. Helpful Answer: "
+rag_template = "Use the following pieces of context to answer the question at the end. " + template + "{context} Question: {question}. Helpful Answer: "
+
+LLM_CHAIN_PROMPT = PromptTemplate(input_variables = ["question"], template = llm_template)
+RAG_CHAIN_PROMPT = PromptTemplate(input_variables = ["context", "question"], template = rag_template)
 
 CHROMA_DIR  = "/data/chroma"
 YOUTUBE_DIR = "/data/youtube"
@@ -115,11 +116,11 @@ def rag_chain(llm, prompt, db):
     completion = rag_chain({"query": prompt})
     return completion
 
-def wandb_trace(rag_option, prompt, prompt_template, result, completion, chain_name, status_msg, start_time_ms, end_time_ms):
+def wandb_trace(rag_option, prompt, completion, status_msg, start_time_ms, end_time_ms):
     wandb.init(project = "openai-llm-rag")
     trace = Trace(
-        name = chain_name,
         kind = "chain",
+        name = "LLMChain" if (rag_option == "Off") else "RetrievalQA",
         status_code = "SUCCESS" if (str(status_msg) == "") else "ERROR",
         status_message = str(status_msg),
         metadata={
@@ -129,11 +130,12 @@ def wandb_trace(rag_option, prompt, prompt_template, result, completion, chain_n
             "model": config["model"],
             "temperature": config["temperature"],
         },
+        inputs = {"rag_option": rag_option if (str(status_msg) == "") else "",
+                  "prompt": str(prompt if (str(status_msg) == "") else ""), 
+                  "prompt_template": str((llm_template if (rag_option == "Off") else rag_template) if (str(status_msg) == "") else "")},
+        outputs = {"completion": str(completion)},
         start_time_ms = start_time_ms,
-        end_time_ms = end_time_ms,
-        inputs = {"rag_option": rag_option, "prompt": str(prompt), "prompt_template": str(prompt_template)},
-        outputs = {"result": str(result), "completion": str(completion)},
-        #model_dict = {"llm": str(llm)}
+        end_time_ms = end_time_ms}
     )
     trace.log("test")
     wandb.finish()
@@ -147,8 +149,6 @@ def invoke(openai_api_key, rag_option, prompt):
         raise gr.Error("Prompt is required.")
     completion = ""
     result = ""
-    prompt_template = ""
-    chain_name = ""
     status_msg = ""
     try:
         start_time_ms = round(time.time() * 1000)
@@ -161,51 +161,22 @@ def invoke(openai_api_key, rag_option, prompt):
             db = document_retrieval_chroma(llm, prompt)
             completion = rag_chain(llm, prompt, db)
             result = completion["result"]
-            prompt_template = rag_template
-            chain_name = "RetrievalQA"
         elif (rag_option == "MongoDB"):
             #splits = document_loading_splitting()
             #document_storage_mongodb(splits)
             db = document_retrieval_mongodb(llm, prompt)
             completion = rag_chain(llm, prompt, db)
             result = completion["result"]
-            prompt_template = rag_template
-            chain_name = "RetrievalQA"
         else:
             result = llm_chain(llm, prompt)
-            prompt_template = llm_template
-            chain_name = "LLMChain"
+            completion = result
     except Exception as e:
         status_msg = e
         raise gr.Error(e)
     finally:
         end_time_ms = round(time.time() * 1000)
-        wandb_trace(rag_option, prompt, prompt_template, result, completion, chain_name, status_msg, start_time_ms, end_time_ms)
+        wandb_trace(rag_option, prompt, completion, status_msg, start_time_ms, end_time_ms)
     return result
-
-description = """<strong>Overview:</strong> Context-aware multimodal reasoning application using a <strong>large language model (LLM)</strong> with 
-                 <strong>retrieval augmented generation (RAG)</strong>. 
-                 See the <a href='https://huggingface.co/spaces/bstraehle/openai-llm-rag/blob/main/openai-llm-rag.png'>architecture diagram</a>.\n\n
-                 <strong>Instructions:</strong> Enter an OpenAI API key and perform text generation use cases on <a href='""" + YOUTUBE_URL_1 + """'>YouTube</a>, 
-                 <a href='""" + PDF_URL + """'>PDF</a>, and <a href='""" + WEB_URL + """'>web</a> data published after LLM knowledge cutoff (example: GPT-4 data).
-                 <ul style="list-style-type:square;">
-                 <li>Set "Retrieval Augmented Generation" to "<strong>Off</strong>" and submit prompt "What is GPT-4?" The <strong>LLM without RAG</strong> does not know the answer.</li>
-                 <li>Set "Retrieval Augmented Generation" to "<strong>Chroma</strong>" or "<strong>MongoDB</strong>" and experiment with prompts. The <strong>LLM with RAG</strong> knows the answer:</li>
-                 <ol>
-                 <li>What are GPT-4's media capabilities in 5 emojis and 1 sentence?</li>
-                 <li>List GPT-4's exam scores and benchmark results.</li>
-                 <li>Compare GPT-4 to GPT-3.5 in markdown table format.</li>
-                 <li>Write a Python program that calls the GPT-4 API.</li>
-                 <li>What is the GPT-4 API's cost and rate limit? Answer in English, Arabic, Chinese, Hindi, and Russian in JSON format.</li>
-                 </ol>
-                 </ul>\n\n
-                 <strong>Technology:</strong> <a href='https://www.gradio.app/'>Gradio</a> UI using the <a href='https://openai.com/'>OpenAI</a> API and 
-                 AI-native <a href='https://www.trychroma.com/'>Chroma</a> embedding database or 
-                 <a href='https://www.mongodb.com/blog/post/introducing-atlas-vector-search-build-intelligent-applications-semantic-search-ai'>MongoDB</a> vector search. 
-                 <strong>Speech-to-text</strong> via <a href='https://openai.com/research/whisper'>whisper-1</a> model, <strong>text embedding</strong> via 
-                 <a href='https://openai.com/blog/new-and-improved-embedding-model'>text-embedding-ada-002</a> model, and <strong>text generation</strong> via 
-                 <a href='""" + WEB_URL + """'>gpt-4</a> model. Implementation via AI-first <a href='https://www.langchain.com/'>LangChain</a> toolkit. 
-                 RAG evaluation via <a href='https://wandb.ai/bstraehle/openai-llm-rag/workspace'>Weights & Biases</a>."""
 
 gr.close_all()
 demo = gr.Interface(fn=invoke, 
