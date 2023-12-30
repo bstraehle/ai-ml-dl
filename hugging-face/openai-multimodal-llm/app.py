00c8b4e34b5f4c062490699dac28f6fbed818aa7
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2023-12-29 17:19:28 -0800

    Update app.py

--- hugging-face/openai-multimodal-llm/app.py
+++ hugging-face/openai-multimodal-llm/app.py
@@ -52,7 +52,7 @@ gr.close_all()
 
 demo = gr.Interface(fn = invoke, 
                     inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
-                              gr.Textbox(label = "Prompt", lines = 1, value = "How to prepare this dish? What wine to serve? What music to play?"),
+                              gr.Textbox(label = "Prompt", lines = 1, value = "How to prepare this dish? What wine to serve with it? What music to play?"),
                               gr.Image(type = "filepath", sources = ["upload"], value = "https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/openai-multimodal-llm/chiles-en-nogada.jpg")],
                     outputs = [gr.Textbox(label = "Completion", lines = 1)],
                     title = "Generative AI - Multimodal LLM",
