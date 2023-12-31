7d54ff6cb3bd806193434702cd97d7f561a23763
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2023-12-31 10:20:06 -0800

    Update app.py

--- hugging-face/openai-multimodal-llm/app.py
+++ hugging-face/openai-multimodal-llm/app.py
@@ -51,14 +51,13 @@ description = """<a href='https://www.gradio.app/'>Gradio</a> UI using the <a hr
 gr.close_all()
 
 demo = gr.Interface(fn = invoke, 
-                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1, value = "sk-"),
-                              gr.Textbox(label = "Prompt", lines = 1, value = "Describe the diagram."),
-                              gr.Image(label = "Image", type = "filepath", sources = ["upload"], value = "https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/openai-multimodal-llm/img1.png"),],
+                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
+                              gr.Textbox(label = "Prompt", lines = 1),
+                              gr.Image(label = "Image", type = "filepath", sources = ["upload"]),],
                     outputs = [gr.Textbox(label = "Completion", lines = 1),],
                     title = "Generative AI - Multimodal LLM",
                     description = description,
-                    examples = [["sk-", "Describe the diagram.", "https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/openai-multimodal-llm/img1.png"],
-                                ["sk-", "Write a poem about the dish in Spanish.", "https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/openai-multimodal-llm/img2.jpg"],],
+                    examples = [["sk-", "Describe the diagram.", "https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/architecture-openai-llm-rag.png"],],
                                 cache_examples = False)
 
 demo.launch()
