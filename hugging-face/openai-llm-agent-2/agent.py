fe1b4a2ef6ec8089070947f60aa3d09fbc7fb4f3
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-01-02 21:29:46 -0800

    Create agent.py

---
+++ hugging-face/openai-llm-agent-2/agent.py
@@ -0,0 +1,33 @@
+import os
+
+from datetime import date
+from llama_hub.tools.weather import OpenWeatherMapToolSpec
+from llama_index.agent import OpenAIAgent
+from llama_index.llms import OpenAI
+from llama_index.tools import FunctionTool
+
+def date_tool(text: str) -> str:
+    """Returns today's date. Use this for any questions related to knowing today's date. 
+       The input should always be an empty string, and this function will always return today's date. 
+       Any date mathematics should occur outside this function."""
+    return str(date.today())
+
+def invoke_agent(model, temperature, openai_api_key, prompt):
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+
+    llm = OpenAI(
+        model = model,
+        temperature = temperature)
+
+    tool_spec = OpenWeatherMapToolSpec(key = os.environ["OPENWEATHERMAP_API_KEY"])
+    tools = tool_spec.to_tool_list()
+    
+    dt_tool = FunctionTool.from_defaults(fn = date_tool)
+            
+    agent = OpenAIAgent.from_tools(
+        [tools[0], # built-in tools
+         dt_tool], # custom tools
+        llm = llm, 
+        verbose = True)
+
+    return agent.chat(prompt)
