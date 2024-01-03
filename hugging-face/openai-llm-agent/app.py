e18855c6ab63bbc92031e3c920cb948e3d3f98b4
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-01-02 20:37:11 -0800

    Update app.py

--- hugging-face/openai-llm-agent/app.py
+++ hugging-face/openai-llm-agent/app.py
@@ -1,30 +1,19 @@
 import gradio as gr
 import os
 
-from datetime import date
-from langchain.agents import AgentType, initialize_agent, load_tools, tool
-from langchain.chat_models import ChatOpenAI
+from agent import invoke_agent
 from openai import OpenAI
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
-OPENWEATHERMAP_API_KEY = os.environ["OPENWEATHERMAP_API_KEY"]
-
 config = {
     "model": "gpt-4-0613",
-    "temperature": 0,
+    "temperature": 0
 }
 
 AGENT_OFF = False
 AGENT_ON  = True
-
-@tool
-def date_tool(text: str) -> str:
-    """Returns today's date. Use this for any questions related to knowing today's date. 
-       The input should always be an empty string, and this function will always return today's date. 
-       Any date mathematics should occur outside this function."""
-    return str(date.today())
 
 def invoke(openai_api_key, prompt, agent_option):
     if (openai_api_key == ""):
@@ -43,26 +32,15 @@ def invoke(openai_api_key, prompt, agent_option):
             completion = client.chat.completions.create(
                 messages = [{"role": "user", "content": prompt}],
                 model = config["model"],
-                temperature = config["temperature"],)
+                temperature = config["temperature"])
     
             output = completion.choices[0].message.content
         else:
-            llm = ChatOpenAI(
-                model_name = config["model"],
-                openai_api_key = openai_api_key, 
-                temperature = config["temperature"])
-    
-            tools = load_tools(["openweathermap-api"])
-            
-            agent = initialize_agent(
-                tools +      # built-in tools
-                [date_tool], # custom tools
-                llm,
-                agent = AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-                handle_parsing_errors = True,
-                verbose = True)
-
-            completion = agent(prompt)
+            completion = invoke_agent(
+                config["model"], 
+                config["temperature"], 
+                openai_api_key, 
+                prompt)
     
             output = completion["output"]
     except Exception as e:
