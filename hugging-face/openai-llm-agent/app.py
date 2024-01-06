2b44f1c5f48fd06f9191840c6239686f9b2d99f7
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-01-05 23:47:18 -0800

    Update app.py

--- hugging-face/openai-llm-agent/app.py
+++ hugging-face/openai-llm-agent/app.py
@@ -8,14 +8,14 @@ from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
+AGENT_OFF = "Off"
+AGENT_LANGCHAIN  = "LangChain"
+AGENT_LLAMAINDEX = "LlamaIndex"
+
 config = {
     "model": "gpt-4-0613",
     "temperature": 0
 }
-
-AGENT_OFF = "Off"
-AGENT_LANGCHAIN  = "LangChain"
-AGENT_LLAMAINDEX = "LlamaIndex"
 
 def invoke(openai_api_key, prompt, agent_option):
     if (openai_api_key == ""):
@@ -34,21 +34,24 @@ def invoke(openai_api_key, prompt, agent_option):
             completion = agent_langchain(
                 config["model"], 
                 config["temperature"],
-                prompt)
+                prompt
+            )
     
             output = completion["output"]
         elif (agent_option == AGENT_LLAMAINDEX):
             output = agent_llamaindex(
                 config["model"], 
                 config["temperature"],
-                prompt)
+                prompt
+            )
         else:
             client = OpenAI()
     
             completion = client.chat.completions.create(
                 messages = [{"role": "user", "content": prompt}],
                 model = config["model"],
-                temperature = config["temperature"])
+                temperature = config["temperature"]
+            )
     
             output = completion.choices[0].message.content
     except Exception as e:
@@ -60,12 +63,13 @@ def invoke(openai_api_key, prompt, agent_option):
 
 gr.close_all()
 
-demo = gr.Interface(fn = invoke, 
-                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
-                              gr.Textbox(label = "Prompt", lines = 1, value = "How does current weather in San Francisco and Paris compare in metric and imperial system? Answer in JSON format and include today's date."),
-                              gr.Radio([AGENT_OFF, AGENT_LANGCHAIN, AGENT_LLAMAINDEX], label = "Use Agent", value = AGENT_LANGCHAIN)],
-                    outputs = [gr.Textbox(label = "Completion", lines = 1)],
-                    title = "Real-Time Reasoning Application",
-                    description = os.environ["DESCRIPTION"])
+demo = gr.Interface(
+    fn = invoke, 
+    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
+              gr.Textbox(label = "Prompt", lines = 1, value = "How does current weather in San Francisco and Paris compare in metric and imperial system? Answer in JSON format and include today's date."),
+              gr.Radio([AGENT_OFF, AGENT_LANGCHAIN, AGENT_LLAMAINDEX], label = "Use Agent", value = AGENT_LANGCHAIN)],
+    outputs = [gr.Textbox(label = "Completion", lines = 1)],
+    title = "Real-Time Reasoning Application",
+    description = os.environ["DESCRIPTION"])
 
 demo.launch()
