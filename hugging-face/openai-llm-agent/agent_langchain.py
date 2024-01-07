944bca914147a09276e930560b1e35a2084383d3
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-01-07 10:41:09 -0800

    Update agent_langchain.py

--- hugging-face/openai-llm-agent/agent_langchain.py
+++ hugging-face/openai-llm-agent/agent_langchain.py
@@ -5,7 +5,7 @@ from langchain.agents import AgentType, initialize_agent, load_tools, tool
 from langchain.chat_models import ChatOpenAI
 
 @tool
-def date_tool(text: str) -> str:
+def today_tool(text: str) -> str:
     """Returns today's date. Use this for any questions related to knowing today's date. 
        The input should always be an empty string, and this function will always return today's date. 
        Any date mathematics should occur outside this function."""
@@ -22,7 +22,7 @@ def agent_langchain(config, prompt):
             
     agent = initialize_agent(
         tools +      # built-in tools
-        [date_tool], # custom tools
+        [today_tool], # custom tools
         llm,
         agent = AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         handle_parsing_errors = True,
