78e616a46ef6b2bcae8f268436aca16f37d3307e
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-01-06 17:56:33 -0800

    Update trace.py

--- hugging-face/openai-llm-rag/trace.py
+++ hugging-face/openai-llm-rag/trace.py
@@ -4,6 +4,9 @@ from wandb.sdk.data_types.trace_tree import Trace
 
 WANDB_API_KEY = os.environ["WANDB_API_KEY"]
 
+RAG_LANGCHAIN  = "LangChain"
+RAG_LLAMAINDEX = "LlamaIndex"
+
 def trace_wandb(config,
                 rag_option,
                 prompt,
@@ -14,13 +17,21 @@ def trace_wandb(config,
                 start_time_ms,
                 end_time_ms):
     wandb.init(project = "openai-llm-rag")
-    
+
+    if (rag_option == RAG_LANGCHAIN):
+        prompt_template = os.environ["LANGCHAIN_TEMPLATE"]
+    elif (rag_option == RAG_LLAMAINDEX):
+        prompt_template = os.environ["LLAMAINDEX_TEMPLATE"]
+    else:
+        prompt_template = os.environ["TEMPLATE"]
+                    
     trace = Trace(
         kind = "LLM",
         name = "Context-Aware Reasoning Application",
         status_code = "success" if (str(err_msg) == "") else "error",
         status_message = str(err_msg),
         inputs = {"prompt": prompt,
+                  "prompt_template": prompt_template,
                   "rag_option": rag_option,
                   "config": str(config)
                  } if (str(err_msg) == "") else {},
