74d4d3a1c9573dbe16fefe9dca3bbd80b43d0a9e
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-03-09 08:45:45 -0800

    Update app.py

--- hugging-face/transformers/app.py
+++ hugging-face/transformers/app.py
@@ -8,8 +8,9 @@ def exec(input):
     out = pipe(input)
     return out[0]["generated_text"]
 
-desc = """<a href='https://www.gradio.app/'>Gradio</a> UI using the Hugging Face <a href='https://huggingface.co/docs/transformers/en/index'>Transformers</a> library 
-          for image captioning <a href='https://huggingface.co/tasks'>task</a>."""
+desc = """<a href='https://www.gradio.app/'>Gradio</a> UI using the Hugging Face 
+          <a href='https://huggingface.co/docs/transformers/en/index'>Transformers</a> 
+          library for image captioning <a href='https://huggingface.co/tasks'>task</a>."""
 
 demo = gr.Interface(exec,
                     inputs=gr.Image(type="pil",
