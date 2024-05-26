ed489b0d2b4775e43305cf38d218783ceee7aec2
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-26 15:11:02 -0700

    Update app.py

--- hugging-face/agentic-rag/app.py
+++ hugging-face/agentic-rag/app.py
@@ -5,13 +5,13 @@ from crew import get_crew
 
 LLM = "gpt-4o"
 
-def invoke(openai_api_key, topic, word_count=500):
+def invoke(openai_api_key, topic, word_count=1000):
     if (openai_api_key == ""):
         raise gr.Error("OpenAI API Key is required.")
     if (topic == ""):
         raise gr.Error("Topic is required.")
         
-    #agentops.init(os.environ["AGENTOPS_API_KEY"])
+    agentops.init(os.environ["AGENTOPS_API_KEY"])
 
     os.environ["OPENAI_API_KEY"] = openai_api_key
 
@@ -24,7 +24,7 @@ gr.close_all()
 demo = gr.Interface(fn = invoke, 
                     inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
                               gr.Textbox(label = "Topic", value=os.environ["TOPIC"], lines = 1),
-                              gr.Number(label = "Word Count", value=500, minimum=500, maximum=5000)],
+                              gr.Number(label = "Word Count", value=1000, minimum=500, maximum=5000)],
                     outputs = [gr.Markdown(label = "Generated Blog Post", value=os.environ["OUTPUT"])],
                     title = "Multi-Agent RAG: Blog Post Generation",
                     description = os.environ["DESCRIPTION"])
