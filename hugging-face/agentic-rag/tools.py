8995703cff2f40047362ce891f06b423146ed842
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-27 09:59:40 -0700

    Update tools.py

--- hugging-face/agentic-rag/tools.py
+++ hugging-face/agentic-rag/tools.py
@@ -1,4 +1,16 @@
-from crewai_tools import ScrapeWebsiteTool, SerperDevTool
+from crewai_tools import BaseTool, ScrapeWebsiteTool, SerperDevTool
+
+from datetime import date
+
+class TodayTool(BaseTool):
+    name: str ="Today Tool"
+    description: str = ("Gets today's date.")
+    
+    def _run(self) -> str:
+        return (str(date.today()))
+
+def today_tool():
+    return TodayTool()
 
 def search_tool():
     return SerperDevTool()
