f750ff16cf538886571f7e45e71905fa7ce79ddd
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-27 09:59:19 -0700

    Update tasks.py

--- hugging-face/agentic-rag/tasks.py
+++ hugging-face/agentic-rag/tasks.py
@@ -1,23 +1,24 @@
 from crewai import Task
 
-from agents import get_researcher_agent, get_blogger_agent
+from agents import get_researcher_agent, get_writer_agent
 
 def get_research_task(model):
     return Task(
         description=(
-            "1. Search the web for content on topic {topic}.\n"
-            "2. Scrape the 10 most relevant web sites for content, prioritizing research papers."
+            "1. Search the web for content on topic: {topic}, prioritizing research papers.\n"
+            "2. Scrape the 10 most relevant web sites for content."
         ),
-        expected_output="Content on topic {topic}.",
+        expected_output="Content on topic: {topic}.",
         agent=get_researcher_agent(model),
     )
 
-def get_blog_task(model):
+def get_write_task(model):
     return Task(
         description=(
-            "1. Use the context to write a {word_count}-word blog post on topic {topic}.\n"
-            "2. If applicable, add a references section with research papers."
+            "1. Use the context to write a 2000-word article on topic: {topic}.\n"
+            "2. At the beginning of the article, add current date and author: Multi-AI-Agent System.\n"
+            "3. At the end of the article, add a references section with research papers."
         ),
-        expected_output="A {word_count}-word blog post on topic {topic}.",
-        agent=get_blogger_agent(model),
+        expected_output="An article on topic {topic}.",
+        agent=get_writer_agent(model),
     )
