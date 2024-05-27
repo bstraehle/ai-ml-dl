7fdbad4200cf9855c733651eacb9701125e5a44c
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-27 09:58:37 -0700

    Update agents.py

--- hugging-face/agentic-rag/agents.py
+++ hugging-face/agentic-rag/agents.py
@@ -1,26 +1,27 @@
 from crewai import Agent
 from langchain_openai import ChatOpenAI
-from tools import scrape_tool, search_tool
+from tools import scrape_tool, search_tool, today_tool
 
 def get_researcher_agent(model):
     return Agent(
         role="Researcher",
-        goal="Research content on topic {topic}",
+        goal="Research content on topic: {topic}.",
         backstory="You're working on researching content on topic {topic}. "
-                  "Your work is the basis for the Blogger to post on this topic.",
+                  "Your work is the basis for the Writer to write on this topic.",
         llm=ChatOpenAI(model=model, temperature=0.7),
         tools = [search_tool(), scrape_tool()],
         allow_delegation=False,
     	verbose=False
     )
 
-def get_blogger_agent(model):
+def get_writer_agent(model):
     return Agent(
-        role="Blogger",
-        goal="Write a {word_count}-word blog post on topic {topic}",
-        backstory="You're working on writing a {word_count}-word blog post on topic {topic}. "
+        role="Writer",
+        goal="Write an article on topic: {topic}.",
+        backstory="You're working on writing a 2000-word article on topic {topic}. "
                   "You base your writing on the work of the Researcher, who provides context on this topic.",
         llm=ChatOpenAI(model=model, temperature=0.7),
+        tools = [today_tool()],
         allow_delegation=False,
         verbose=False
     )
