d216762501cba18a4cbdcb6f48aff0f6d637de43
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-27 09:59:00 -0700

    Update crew.py

--- hugging-face/agentic-rag/crew.py
+++ hugging-face/agentic-rag/crew.py
@@ -1,14 +1,14 @@
 from crewai import Crew, Process
 from langchain_openai import ChatOpenAI
 
-from agents import get_researcher_agent, get_blogger_agent
-from tasks import get_research_task, get_blog_task
+from agents import get_researcher_agent, get_writer_agent
+from tasks import get_research_task, get_write_task
 
 def get_crew(model):
     return Crew(
-        agents=[get_researcher_agent(model), get_blogger_agent(model)],
-        tasks=[get_research_task(model), get_blog_task(model)],
+        agents=[get_researcher_agent(model), get_writer_agent(model)],
+        tasks=[get_research_task(model), get_write_task(model)],
         manager_llm=ChatOpenAI(model=model, temperature=0.7),
         process=Process.sequential,
-        verbose=True
+        verbose=False
     )
