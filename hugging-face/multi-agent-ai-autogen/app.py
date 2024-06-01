e40a03c576de0e720fd624403c467a407ab082a4
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-06-01 13:55:30 -0700

    Update app.py

--- hugging-face/multi-agent-ai-autogen/app.py
+++ hugging-face/multi-agent-ai-autogen/app.py
@@ -6,19 +6,19 @@ from multi_agent import run_multi_agent
 LLM_WHITE = "gpt-4o"
 LLM_BLACK = "gpt-4o"
 
-def invoke(openai_api_key, num_turns = 2):
+def invoke(openai_api_key, num_moves = 10):
     if (openai_api_key == ""):
         raise gr.Error("OpenAI API Key is required.")
         
     os.environ["OPENAI_API_KEY"] = openai_api_key
-   
-    return run_multi_agent(LLM_WHITE, LLM_BLACK, num_turns)
+
+    return run_multi_agent(LLM_WHITE, LLM_BLACK, num_moves)
 
 gr.close_all()
 
 demo = gr.Interface(fn = invoke, 
                     inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
-                              gr.Number(label = "Number of Turns", value = 2, minimum = 1, maximum = 300)],
+                              gr.Number(label = "Number of Moves", value = 10, minimum = 1, maximum = 50)],
                     outputs = [gr.Markdown(label = "Game", value=os.environ["OUTPUT"], line_breaks=True, sanitize_html=False)],
                     title = "Chess: AI vs. AI",
                     description = os.environ["DESCRIPTION"])
