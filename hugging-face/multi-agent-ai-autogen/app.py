9a1c4f6cfc5b1f358fb94c3295af8704754b2844
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-05-31 17:41:14 -0700

    Create app.py

---
+++ hugging-face/multi-agent-ai-autogen/app.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import os
+
+from multi_agent import run_multi_agent
+
+LLM_WHITE = "gpt-4o"
+LLM_BLACK = "gpt-4o"
+
+def invoke(openai_api_key, num_turns = 2):
+    if (openai_api_key == ""):
+        raise gr.Error("OpenAI API Key is required.")
+        
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+   
+    return run_multi_agent(LLM_WHITE, LLM_BLACK, num_turns)
+
+gr.close_all()
+
+demo = gr.Interface(fn = invoke, 
+                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
+                              gr.Number(label = "Number of Turns", value = 2, minimum = 1, maximum = 300)],
+                    outputs = [gr.Markdown(label = "Game", value=os.environ["OUTPUT"], line_breaks=True, sanitize_html=False)],
+                    title = "♙ AI vs. AI ♟",
+                    description = os.environ["DESCRIPTION"])
+
+demo.launch()
