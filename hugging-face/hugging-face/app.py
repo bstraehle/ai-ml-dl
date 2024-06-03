0cc9c8b148651579c71902a98c4b8fa6f5bc2740
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-06-03 12:03:38 -0700

    Update app.py

--- hugging-face/hugging-face/app.py
+++ hugging-face/hugging-face/app.py
@@ -1,21 +1,21 @@
+import os, threading
 import gradio as gr
 from transformers import pipeline
+
+input_lock = threading.Lock()
 
 pipe = pipeline("image-to-text",
                 model="Salesforce/blip-image-captioning-base")
 
 def exec(input):
-    out = pipe(input)
-    return out[0]["generated_text"]
-
-desc = """<a href='https://www.gradio.app/'>Gradio</a> UI using the Hugging Face 
-          <a href='https://huggingface.co/docs/transformers/en/index'>Transformers</a> library 
-          for image captioning (image-to-text) <a href='https://huggingface.co/tasks'>tasks</a>."""
+    with input_lock:
+        out = pipe(input)
+        return out[0]["generated_text"]
 
 demo = gr.Interface(exec,
                     inputs=gr.Image(type="pil",
                                     value="https://raw.githubusercontent.com/bstraehle/ai-ml-dl/main/hugging-face/hugging-face/beach.jpg"),
-                    outputs="text",
-                    description=desc)
+                    outputs=[gr.Textbox(label = "output", value=os.environ["OUTPUT"])],
+                    description=os.environ["DESCRIPTION"])
 
 demo.launch()
