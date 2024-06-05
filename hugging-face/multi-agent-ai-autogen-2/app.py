b55eee7266ac6486f75ec7d61fd4d38220a7c681
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-06-04 22:54:24 -0700

    Create app.py

---
+++ hugging-face/multi-agent-ai-autogen-2/app.py
@@ -0,0 +1,32 @@
+import gradio as gr
+import datetime, os, threading
+
+from multi_agent import run_multi_agent
+
+lock = threading.Lock()
+
+LLM = "gpt-4o"
+
+def invoke(openai_api_key, task):
+    if not openai_api_key:
+        raise gr.Error("OpenAI API Key is required.")
+
+    if not task:
+        raise gr.Error("Task is required.")
+    
+    with lock:
+        os.environ["OPENAI_API_KEY"] = openai_api_key
+        result = run_multi_agent(LLM, task)
+        del os.environ["OPENAI_API_KEY"]
+        return result
+
+gr.close_all()
+
+demo = gr.Interface(fn = invoke, 
+                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
+                              gr.Textbox(label = "Task", value = f"Today is {datetime.date.today()}. {os.environ['INPUT']}")],
+                    outputs = [gr.Markdown(label = "Output", value = os.environ["OUTPUT"], line_breaks = True, sanitize_html = False)],
+                    title = "Multi-Agent AI: Financial Analysis",
+                    description = os.environ["DESCRIPTION"])
+
+demo.launch()
