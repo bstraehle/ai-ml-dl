555c845d3ca8005e7044637b45f44f245f77ea0d
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-06-09 16:38:19 -0700

    Update app.py

--- hugging-face/multi-agent-ai-langgraph-2/app.py
+++ hugging-face/multi-agent-ai-langgraph-2/app.py
@@ -22,7 +22,7 @@ gr.close_all()
 
 demo = gr.Interface(fn = invoke, 
                     inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
-                              gr.Number(label = "Maximum Number of Moves", value = 10, minimum = 1, maximum = 250)],
+                              gr.Number(label = "Maximum Number of Moves (1-25)", value = 10, minimum = 1, maximum = 25)],
                     outputs = [gr.Markdown(label = "Game", value=os.environ["OUTPUT"], line_breaks=True, sanitize_html=False)],
                     title = "Multi-Agent AI: Chess",
                     description = os.environ["DESCRIPTION"])
