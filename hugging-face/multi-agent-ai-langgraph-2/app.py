c79785eab13b66173db1ce6f0e97e9de0dea6d0c
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-06-12 21:47:52 -0700

    Update app.py

--- hugging-face/multi-agent-ai-langgraph-2/app.py
+++ hugging-face/multi-agent-ai-langgraph-2/app.py
@@ -8,13 +8,17 @@ lock = threading.Lock()
 os.environ["LANGCHAIN_PROJECT"] = "langgraph-multi-agent-chess"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
+LLM_BOARD = "gpt-4o"
+LLM_WHITE = "gpt-4o"
+LLM_BLACK = "gpt-4o"
+
 def invoke(openai_api_key, max_moves = 10):
     if not openai_api_key:
         raise gr.Error("OpenAI API Key is required.")
 
     with lock:
         os.environ["OPENAI_API_KEY"] = openai_api_key
-        result = run_multi_agent(max_moves)
+        result = run_multi_agent(LLM_BOARD, LLM_WHITE, LLM_BLACK, max_moves)
         del os.environ["OPENAI_API_KEY"]
         return result
 
