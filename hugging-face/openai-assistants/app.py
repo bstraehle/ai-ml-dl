894ab624e7594ba2cd0d691c47799f2bb9260f6b
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-08-19 22:26:41 -0700

    Update app.py

--- hugging-face/openai-assistants/app.py
+++ hugging-face/openai-assistants/app.py
@@ -1,8 +1,7 @@
 # TODO:
 #
-# 1. Gradio session / multi-user thread
-# 2. Function calling - https://platform.openai.com/docs/assistants/tools/function-calling
-#    - get_stock_price - yfinance
+# 1. Function calling - https://platform.openai.com/docs/assistants/tools/function-calling
+# 2. Gradio session / multi-user thread
 
 # Reference:
 #
@@ -13,24 +12,36 @@
 # https://platform.openai.com/docs/assistants/tools
 
 import gradio as gr
+import pandas as pd
+import yfinance as yf
+
 import json, openai, os, time
 
 from datetime import date
 from openai import OpenAI
+from typing import List
 from utils import function_to_schema, show_json
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 assistant, thread = None, None
 
-def today_tool(text: str) -> str:
-    """Returns today's date. Use this for any questions related to knowing today's date. 
-       The input should always be an empty string, and this function will always return today's date. 
-       Any date mathematics should occur outside this function."""
+def today_tool() -> str:
+    """Returns today's date. Use this function for any questions related to knowing today's date. 
+       There is no input. This function always returns today's date."""
     return str(date.today())
+
+def yf_download_tool(tickers: List[str], start_date: date, end_date: date) -> pd.DataFrame:
+    """Returns historical stock data for given tickers from a start date to an end date 
+       using the yfinance library download function. 
+       Use this function for any questions related to getting historical stock data. 
+       The input should be the tickers as a List of strings, a start date, and an end date. 
+       This function always returns a pandas DataFrame."""
+    return yf.download(tickers, start=start_date, end=end_date)
 
 tools = {
     "today_tool": today_tool,
+    "yf_download_tool": yf_download_tool,
 }
 
 def create_assistant(client):
@@ -39,32 +50,33 @@ def create_assistant(client):
         instructions=(
                          "You are a Python programming language expert that "
                          "generates Pylint-compliant code and explains it. "
-                         "Only execute code when explicitly asked to."
+                         "Execute code when explicitly asked to."
                      ),
         model="gpt-4o",
         tools=[
             {"type": "code_interpreter"},
             {"type": "function", "function": function_to_schema(today_tool)},
+            {"type": "function", "function": function_to_schema(yf_download_tool)},
         ],
     )
     
-    #show_json("assistant", assistant)
+    show_json("assistant", assistant)
     
     return assistant
 
 def load_assistant(client):
-    ASSISTANT_ID = "asst_TpZgBd2QYaxUxCwUy8J9m3Bq"
+    ASSISTANT_ID = "asst_ypbcWnilAd60bc2DQ8haDL5P"
     
     assistant = client.beta.assistants.retrieve(ASSISTANT_ID)
 
-    #show_json("assistant", assistant)
+    show_json("assistant", assistant)
     
     return assistant
 
 def create_thread(client):
     thread = client.beta.threads.create()
     
-    #show_json("thread", thread)
+    show_json("thread", thread)
     
     return thread
 
@@ -75,7 +87,7 @@ def create_message(client, thread, msg):
         content=msg,
     )
     
-    #show_json("message", message)
+    show_json("message", message)
     
     return message
 
@@ -83,22 +95,24 @@ def create_run(client, assistant, thread):
     run = client.beta.threads.runs.create(
         assistant_id=assistant.id,
         thread_id=thread.id,
+        parallel_tool_calls=False,
     )
     
-    #show_json("run", run)
+    show_json("run", run)
     
     return run
 
 def wait_on_run(client, thread, run):
     while run.status == "queued" or run.status == "in_progress":
+        print("### " + run.status)
         run = client.beta.threads.runs.retrieve(
             thread_id=thread.id,
             run_id=run.id,
         )
     
-        time.sleep(0.25)
+        time.sleep(0.5)
     
-    #show_json("run", run)
+    show_json("run", run)
     
     return run
 
@@ -109,21 +123,21 @@ def get_run_steps(client, thread, run):
         order="asc",
     )
 
-    #show_json("run_steps", run_steps)
+    show_json("run_steps", run_steps)
     
     return run_steps
 
 def execute_tool_call(tool_call):
     name = tool_call.function.name
     args = json.loads(tool_call.function.arguments)
-    
+
     return tools[name](**args)
 
 def execute_tool_calls(run_steps):
     run_step_details = []
 
-    tool_call_id = ""
-    tool_call_result = ""
+    tool_call_ids = []
+    tool_call_results = []
     
     for step in run_steps.data:
         step_details = step.step_details
@@ -135,17 +149,17 @@ def execute_tool_calls(run_steps):
                 show_json("tool_call", tool_call)
                 
                 if hasattr(tool_call, "function"):
-                    tool_call_id = tool_call.id
-                    tool_call_result = execute_tool_call(tool_call)
+                    tool_call_ids.append(tool_call.id)
+                    tool_call_results.append(execute_tool_call(tool_call))
 
-    return tool_call_id, tool_call_result
+    return tool_call_ids, tool_call_results
 
 def get_messages(client, thread):
     messages = client.beta.threads.messages.list(
         thread_id=thread.id
     )
     
-    #show_json("messages", messages)
+    show_json("messages", messages)
     
     return messages
                         
@@ -163,6 +177,39 @@ def extract_content_values(data):
     
     return text_values, image_values
 
+###
+def generate_tool_outputs(tool_call_ids, tool_call_results):
+    tool_outputs = []
+    
+    for tool_call_id, tool_call_result in zip(tool_call_ids, tool_call_results):
+        tool_output = {}
+        
+        try:
+            tool_output = {
+                "tool_call_id": tool_call_id,
+                "output": tool_call_result.to_json()
+            }
+
+            print("###")
+            print(tool_call_id)
+            print(tool_call_result.to_json())
+            print("###")
+        except AttributeError:
+            tool_output = {
+                "tool_call_id": tool_call_id,
+                "output": tool_call_result
+            }
+
+            print("###")
+            print(tool_call_id)
+            print(tool_call_result)
+            print("###")
+            
+        tool_outputs.append(tool_output)
+    
+    return tool_outputs
+###
+
 def chat(message, history):
     if not message:
         raise gr.Error("Message is required.")
@@ -170,7 +217,7 @@ def chat(message, history):
     global client, assistant, thread     
     
     if assistant == None:
-        assistant = create_assistant(client)
+        assistant = load_assistant(client)
     
     if thread == None or len(history) == 0:
         thread = create_thread(client)
@@ -181,28 +228,61 @@ def chat(message, history):
     run = wait_on_run(client, thread, run)
 
     run_steps = get_run_steps(client, thread, run)
-    
-    tool_call_id, tool_call_result = execute_tool_calls(run_steps)
+
+    tool_call_ids, tool_call_results = execute_tool_calls(run_steps)
 
     ### TODO
-    if tool_call_result:
-        print("### tool_call_id=" + tool_call_id)
-        print("### tool_call_result=" + tool_call_result)
-
+    print("###")
+    print(len(tool_call_ids))
+    print(tool_call_ids)
+    print(tool_call_ids[0])
+    print(tool_call_results)
+    print(tool_call_results[0])
+    print("###")
+    
+    if tool_call_ids[0]:
+        # https://platform.openai.com/docs/api-reference/runs/submitToolOutputs
         run = client.beta.threads.runs.submit_tool_outputs(
             thread_id=thread.id,
             run_id=run.id,
+            #tool_outputs=generate_tool_outputs(tool_call_ids, tool_call_results)
             tool_outputs=[
                 {
-                  "tool_call_id": tool_call_id,
-                  "output": tool_call_result
+                    "tool_call_id": tool_call_ids[0],
+                    "output": tool_call_results[0]
                 }
             ]
         )
     
         run = wait_on_run(client, thread, run)
         run_steps = get_run_steps(client, thread, run)
-        tool_call_id, tool_call_result = execute_tool_calls(run_steps)
+    ###
+    tool_call_ids, tool_call_results = execute_tool_calls(run_steps)
+
+    print("###")
+    print(len(tool_call_ids))
+    print(tool_call_ids)
+    print(tool_call_ids[1])
+    print(tool_call_results)
+    print(tool_call_results[1])
+    print("###")
+    
+    if tool_call_ids[1]:
+        # https://platform.openai.com/docs/api-reference/runs/submitToolOutputs
+        run = client.beta.threads.runs.submit_tool_outputs(
+            thread_id=thread.id,
+            run_id=run.id,
+            #tool_outputs=generate_tool_outputs(tool_call_ids, tool_call_results)
+            tool_outputs=[
+                {
+                    "tool_call_id": tool_call_ids[1],
+                    "output": tool_call_results[1].to_json()
+                }
+            ]
+        )
+    
+        run = wait_on_run(client, thread, run)
+        run_steps = get_run_steps(client, thread, run)    
     ###
     
     messages = get_messages(client, thread)
@@ -214,7 +294,8 @@ def chat(message, history):
     if len(image_values) > 0:
         download_link = f"<p>Download: https://platform.openai.com/storage/files/{image_values[0]}</p>"
     
-    return f"{text_values[0]}{download_link}"
+    #return f"{text_values[0]}{download_link}"
+    return f"{'<br />'.join(text_values)}{download_link}"
 
 gr.ChatInterface(
         fn=chat,
@@ -231,7 +312,7 @@ gr.ChatInterface(
                   ["Fix: x = [5, 2, 1, 3, 4]; print(x.sort())"],
                   ["Optimize: x = []; for i in range(0, 10000): x.append(i)"],
                   ["Execute: First 25 Fibbonaci numbers"],
-                  ["Get today's date, then generate a chart using mock data, showing stock gain YTD for NVDA, MSFT, AAPL, and GOOG, x-axis is 'Day' and y-axis is 'YTD Gain %'"]
+                  ["Create a plot showing stock gain QTD for NVDA and MSFT, x-axis is 'Day' and y-axis is 'QTD Gain %'"]
                  ],
         cache_examples=False,
     ).launch()
