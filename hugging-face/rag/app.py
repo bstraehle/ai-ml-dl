9ae36d5140089393fc0e9ca767804c3621f2b645
Bernd Straehle <50965537+bstraehle@users.noreply.github.com>
2024-09-03 06:22:26 -0700

    Update app.py

--- hugging-face/rag/app.py
+++ hugging-face/rag/app.py
@@ -4,7 +4,7 @@ import logging, os, sys, threading, time
 from dotenv import load_dotenv, find_dotenv
 
 from rag_langchain import LangChainRAG
-from rag_llamaindex import LlamaIndexRAG
+#from rag_llamaindex import LlamaIndexRAG
 from trace import trace_wandb
 
 lock = threading.Lock()
@@ -29,11 +29,11 @@ logging.basicConfig(stream = sys.stdout, level = logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream = sys.stdout))
 
 def invoke(openai_api_key, prompt, rag_option):
-    if (openai_api_key == ""):
+    if not openai_api_key:
         raise gr.Error("OpenAI API Key is required.")
-    if (prompt == ""):
+    if not prompt:
         raise gr.Error("Prompt is required.")
-    if (rag_option is None):
+    if not rag_option:
         raise gr.Error("Retrieval-Augmented Generation is required.")
 
     with lock:
@@ -43,9 +43,9 @@ def invoke(openai_api_key, prompt, rag_option):
             if (rag_option == RAG_LANGCHAIN):
                 rag = LangChainRAG()
                 rag.ingestion(config)
-            elif (rag_option == RAG_LLAMAINDEX):
-                rag = LlamaIndexRAG()
-                rag.ingestion(config)
+            #elif (rag_option == RAG_LLAMAINDEX):
+            #    rag = LlamaIndexRAG()
+            #    rag.ingestion(config)
     
         completion = ""
         result = ""
@@ -59,9 +59,9 @@ def invoke(openai_api_key, prompt, rag_option):
                 rag = LangChainRAG()
                 completion, callback = rag.rag_chain(config, prompt)
                 result = completion["result"]
-            elif (rag_option == RAG_LLAMAINDEX):
-                rag = LlamaIndexRAG()
-                result, callback = rag.retrieval(config, prompt)
+            #elif (rag_option == RAG_LLAMAINDEX):
+            #    rag = LlamaIndexRAG()
+            #    result, callback = rag.retrieval(config, prompt)
             else:
                 rag = LangChainRAG()
                 completion, callback = rag.llm_chain(config, prompt)
@@ -86,7 +86,11 @@ def invoke(openai_api_key, prompt, rag_option):
             )
 
             del os.environ["OPENAI_API_KEY"]
-    
+
+        print("###")
+        print(result)
+        print("###")
+        
         return result
 
 gr.close_all()
@@ -94,16 +98,16 @@ gr.close_all()
 demo = gr.Interface(
     fn = invoke, 
     inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1), 
-              gr.Textbox(label = "Prompt", value = "List GPT-4's exam scores and benchmark results.", lines = 1),
+              gr.Textbox(label = "Prompt", value = "What is the GPT-4 API's cost and rate limit? Answer in English, Arabic, Chinese, Hindi, and Russian in JSON format.", lines = 1),
               gr.Radio([RAG_OFF, RAG_LANGCHAIN, RAG_LLAMAINDEX], label = "Retrieval-Augmented Generation", value = RAG_LANGCHAIN)],
-    outputs = [gr.Textbox(label = "Completion")],
+    outputs = [gr.Textbox(label = "Completion", value = os.environ["COMPLETION"])],
     title = "Context-Aware Reasoning Application",
     description = os.environ["DESCRIPTION"],
-    examples = [["sk-<BringYourOwn>", "What are GPT-4's media capabilities in 5 emojis and 1 sentence?", RAG_LLAMAINDEX],
+    examples = [["sk-<BringYourOwn>", "What are GPT-4's media capabilities in 5 emojis and 1 sentence?", RAG_LANGCHAIN],
                 ["sk-<BringYourOwn>", "List GPT-4's exam scores and benchmark results.", RAG_LANGCHAIN],
-                ["sk-<BringYourOwn>", "Compare GPT-4 to GPT-3.5 in markdown table format.", RAG_LLAMAINDEX],
+                ["sk-<BringYourOwn>", "Compare GPT-4 to GPT-3.5 in markdown table format.", RAG_LANGCHAIN],
                 ["sk-<BringYourOwn>", "Write a Python program that calls the GPT-4 API.", RAG_LANGCHAIN],
-                ["sk-<BringYourOwn>", "What is the GPT-4 API's cost and rate limit? Answer in English, Arabic, Chinese, Hindi, and Russian in JSON format.", RAG_LLAMAINDEX]],
+                ["sk-<BringYourOwn>", "What is the GPT-4 API's cost and rate limit? Answer in English, Arabic, Chinese, Hindi, and Russian in JSON format.", RAG_LANGCHAIN]],
                cache_examples = False
 )
 
