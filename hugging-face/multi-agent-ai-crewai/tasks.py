f454855eee02a6c2591ffa21c125a1ee37874a77
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2024-10-06 12:53:01 -0700

    Update tasks.py

--- hugging-face/multi-agent-ai-crewai/tasks.py
+++ hugging-face/multi-agent-ai-crewai/tasks.py
@@ -2,23 +2,24 @@ from crewai import Task
 
 from agents import get_researcher_agent, get_writer_agent
 
-def get_research_task(model, temperature, verbose):
+def get_research_task(model, verbose):
     return Task(
         description=(
-            "1. Search the web for content on topic: {topic}, prioritizing research papers.\n"
+            "1. Search the web for content on topic: {topic}.\n"
             "2. Scrape the 10 most relevant web sites for content."
         ),
         expected_output="Content on topic: {topic}.",
-        agent=get_researcher_agent(model, temperature, verbose),
+        agent=get_researcher_agent(model, verbose),
     )
 
-def get_write_task(model, temperature, verbose):
+def get_write_task(model, verbose):
     return Task(
         description=(
-            "1. Use the context to write a 2000-word article on topic '{topic}' in markdown format. Omit the triple backticks.\n"
-            "2. At the beginning of the article, add current date and author: Multi-Agent AI System.\n"
-            "3. At the end of the article, add a references section with links to the research papers."
+            "1. Use the context to write an in-depth article on topic: {topic}.\n"
+            "2. The output must be in markdown format (omit the triple backticks).\n"
+            "3. At the beginning of the article, add the current date and author: Multi-Agent AI System.\n"
+            "4. Also at the beginning of the article, add a references section with links to relevant content."
         ),
         expected_output="An article on topic {topic}.",
-        agent=get_writer_agent(model, temperature, verbose),
+        agent=get_writer_agent(model, verbose),
     )
