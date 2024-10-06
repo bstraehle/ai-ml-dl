b14fe635991c638bb96f94746db527a96b2c81b7
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2024-10-06 12:52:38 -0700

    Update crew.py

--- hugging-face/multi-agent-ai-crewai/crew.py
+++ hugging-face/multi-agent-ai-crewai/crew.py
@@ -4,11 +4,13 @@ from langchain_openai import ChatOpenAI
 from agents import get_researcher_agent, get_writer_agent
 from tasks import get_research_task, get_write_task
 
-def get_crew(model):
+def get_crew(model, verbose):
     return Crew(
-        agents=[get_researcher_agent(model), get_writer_agent(model)],
-        tasks=[get_research_task(model), get_write_task(model)],
-        manager_llm=ChatOpenAI(model=model, temperature=0.7),
+        agents=[get_researcher_agent(model, verbose), 
+                get_writer_agent(model, verbose)],
+        tasks=[get_research_task(model, verbose), 
+               get_write_task(model, verbose)],
+        manager_llm=ChatOpenAI(model=model),
         process=Process.sequential,
-        verbose=False
+        verbose=verbose
     )
