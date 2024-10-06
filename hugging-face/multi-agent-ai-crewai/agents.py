3911b6ceb49124cfa596985c35951b19b45601b9
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2024-10-06 12:52:17 -0700

    Update agents.py

--- hugging-face/multi-agent-ai-crewai/agents.py
+++ hugging-face/multi-agent-ai-crewai/agents.py
@@ -2,26 +2,26 @@ from crewai import Agent
 from langchain_openai import ChatOpenAI
 from tools import scrape_tool, search_tool, today_tool
 
-def get_researcher_agent(model):
+def get_researcher_agent(model, verbose):
     return Agent(
         role="Researcher",
         goal="Research content on topic: {topic}.",
-        backstory="You're working on researching content on topic {topic}. "
+        backstory="You're working on researching content on topic: {topic}. "
                   "Your work is the basis for the Writer to write on this topic.",
-        llm=ChatOpenAI(model=model, temperature=0.7),
+        llm=ChatOpenAI(model=model),
         tools = [search_tool(), scrape_tool()],
         allow_delegation=False,
-    	verbose=False
+    	verbose=verbose
     )
 
-def get_writer_agent(model):
+def get_writer_agent(model, verbose):
     return Agent(
         role="Writer",
         goal="Write an article on topic: {topic}.",
-        backstory="You're working on writing a 2000-word article on topic {topic}. "
+        backstory="You're working on writing an article on topic: {topic}. "
                   "You base your writing on the work of the Researcher, who provides context on this topic.",
-        llm=ChatOpenAI(model=model, temperature=0.7),
+        llm=ChatOpenAI(model=model),
         tools = [today_tool()],
         allow_delegation=False,
-        verbose=False
+        verbose=verbose
     )
