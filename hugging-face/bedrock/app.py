ce7ca4d7559fe7a8b0fc390834d12f1b559d7424
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2025-01-12 19:36:47 -0800

    Update app.py

--- hugging-face/bedrock/app.py
+++ hugging-face/bedrock/app.py
@@ -8,8 +8,8 @@ aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
 aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
 
 config = {
-    "max_tokens_to_sample": 1000,
-    "model": "anthropic.claude-v2",
+    "max_tokens": 1000,
+    "model": "anthropic.claude-3-opus-20240229-v1:0",
     "temperature": 0,
     "top_k": 250,
     "top_p": 0.999,
@@ -23,29 +23,36 @@ bedrock_runtime = boto3.client(
 )
 
 def invoke(prompt):
-    if (prompt == ""):
+    if not prompt:
         raise gr.Error("Prompt is required.")
-        
+
+    raise gr.Error("Please clone and bring your own credentials.")
+    
     completion = ""
     
     try:
-        body = json.dumps({"prompt": "Human: " + prompt + "Assistant: ",
-                           "max_tokens_to_sample": config["max_tokens_to_sample"],
-                           "temperature": config["temperature"],
-                           "top_k": config["top_k"],
-                           "top_p": config["top_p"],
-                           "stop_sequences": ["Human: "]})
-        modelId = config["model"]
-        accept = "application/json"
-        contentType = "application/json"
+        body = {
+            "anthropic_version": "bedrock-2023-05-31",
+            "messages": [
+                {"role": "user", "content": [{"type": "text", "text": prompt}]},
+            ],
+            "system": "You are a honest, helpful, and harmless bot."
+        }
+        model_id = config["model"]
+        model_kwargs =  { 
+            "max_tokens": config["max_tokens"],
+            "stop_sequences": ["\n\nHuman"],
+            "temperature": config["temperature"],
+            "top_k": config["top_k"],
+            "top_p": config["top_p"]
+        }
+        body.update(model_kwargs)
         
-        response = bedrock_runtime.invoke_model(body = body, 
-                                                modelId = modelId, 
-                                                accept = accept, 
-                                                contentType = contentType)
+        response = bedrock_runtime.invoke_model(modelId=model_id,
+                                                body=json.dumps(body))
         
         response_body = json.loads(response.get("body").read())
-        completion = response_body["completion"]
+        completion = response_body.get("content", [])[0].get("text", "")
     except Exception as e:
         completion = e
         
@@ -53,15 +60,14 @@ def invoke(prompt):
     
     return completion
 
-description = """<a href='https://www.gradio.app/'>Gradio</a> UI using the <a href='https://aws.amazon.com/bedrock/'>Amazon Bedrock</a> API 
-                 with <a href='https://www.anthropic.com/'>Anthropic</a> Claude 2 model."""
+description = """<a href='https://www.gradio.app/'>Gradio</a> UI using the <a href='https://aws.amazon.com/bedrock/'>Amazon Bedrock</a> SDK 
+                 with <a href='https://www.anthropic.com/'>Anthropic</a> Claude 3 model."""
 
 gr.close_all()
 
 demo = gr.Interface(fn = invoke, 
-                    inputs = [gr.Textbox(label = "Prompt", lines = 1)],
+                    inputs = [gr.Textbox(label = "Prompt", value = "If I dry one shirt in the sun, it takes 1 hour. How long do 3 shirts take?", lines = 1)],
                     outputs = [gr.Textbox(label = "Completion", lines = 1)],
-                    title = "Generative AI - LLM",
                     description = description)
 
 demo.launch()
