aaace6dd93178676d2cbf5aaa2d018506d35e65f
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2025-05-10 15:07:01 -0700

    Update app.py

--- hugging-face/multi-agent-ai-langgraph/app.py
+++ hugging-face/multi-agent-ai-langgraph/app.py
@@ -8,7 +8,7 @@ lock = threading.Lock()
 os.environ["LANGCHAIN_PROJECT"] = "langgraph-multi-agent"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
-LLM = "gpt-4o"
+LLM = "gpt-4.1"
 
 def invoke(openai_api_key, topic):
     if not openai_api_key:
@@ -22,13 +22,37 @@ def invoke(openai_api_key, topic):
         del os.environ["OPENAI_API_KEY"]
         return article
 
+def clear():
+    return ""
+
 gr.close_all()
 
-demo = gr.Interface(fn = invoke, 
-                    inputs = [gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1),
-                              gr.Textbox(label = "Topic", value=os.environ["TOPIC"], lines = 1)],
-                    outputs = [gr.Markdown(label = "Article", value=os.environ["OUTPUT"])],
-                    title = "Multi-Agent AI: Article Writing",
-                    description = os.environ["DESCRIPTION"])
+with gr.Blocks() as assistant:
+    gr.Markdown("## Multi-Agent AI: Article Writing")
+    gr.Markdown(os.environ.get("DESCRIPTION"))
 
-demo.launch()
+    with gr.Row():
+        with gr.Column(scale=1):
+            with gr.Row():
+                openai_api_key = gr.Textbox(label = "OpenAI API Key", type = "password", lines = 1)
+                topic = gr.Textbox(label = "Topic", value=os.environ["TOPIC"], lines = 1)            
+            with gr.Row():
+                clear_btn = gr.ClearButton(
+                    components=[openai_api_key, topic]
+                )
+                submit_btn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=3):
+            article = gr.Markdown(label = "Article", value=os.environ["OUTPUT"], line_breaks = True, sanitize_html = False)
+
+    clear_btn.click(
+        fn=clear,
+        outputs=article
+    )
+    
+    submit_btn.click(
+        fn=invoke,
+        inputs=[openai_api_key, topic],
+        outputs=article
+    )
+
+assistant.launch()
