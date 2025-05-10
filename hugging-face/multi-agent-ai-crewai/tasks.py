2b50f50169c46ff8657161944c77447263fcebc8
B. Straehle <50965537+bstraehle@users.noreply.github.com>
2025-05-10 14:57:25 -0700

    Update tasks.py

--- hugging-face/multi-agent-ai-crewai/tasks.py
+++ hugging-face/multi-agent-ai-crewai/tasks.py
@@ -6,7 +6,7 @@ def get_research_task(model, verbose):
     return Task(
         description=(
             "1. Search the web for content on topic: {topic}.\n"
-            "2. Scrape the 10 most relevant web sites for content."
+            "2. Scrape the 25 most relevant web sites for content."
         ),
         expected_output="Content on topic: {topic}.",
         agent=get_researcher_agent(model, verbose),
@@ -15,10 +15,11 @@ def get_research_task(model, verbose):
 def get_write_task(model, verbose):
     return Task(
         description=(
-            "1. Use the context to write an in-depth article on topic: {topic}.\n"
-            "2. The output must be in markdown format (omit the triple backticks).\n"
-            "3. At the beginning of the article, add the current date and author: Multi-Agent AI System.\n"
-            "4. Also at the beginning of the article, add a references section with links to relevant content."
+            "1. Use the context to write a long and in-depth article on topic: {topic}.\n"
+            "2. The output must be in markdown format (omit the triple backticks), including lists.\n"
+            "3. At the beginning of the article below the title, add the current date and author: Multi-Agent AI System.\n"
+            "4. At the end of the article, add a references section with distinct links to content provided by the Researcher.\n"
+            "5. Include inline references to the links, for example: [1]."
         ),
         expected_output="An article on topic {topic}.",
         agent=get_writer_agent(model, verbose),
